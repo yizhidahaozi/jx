3691f207ab0fcc9e7b83e5512b3319a69b729905
雨呢 <85211716+cluntop@users.noreply.github.com>
2025-01-25 14:15:09 +0800

    更新 tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# https://ghfast.top/
+# https://ghfast.top/ # sleep 1
 clun_download() {
-cd ~ && sleep 1 && curl -sS -o clun_tcp.sh https://raw.githubusercontent.com/cluntop/sh/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
+cd ~ && curl -sS -o clun_tcp.sh https://raw.githubusercontent.com/cluntop/sh/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
 } && clun_download
