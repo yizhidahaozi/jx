2c81634bc550864de196315aad535d34d2d2cabf
雨呢 <85211716+cluntop@users.noreply.github.com>
2025-01-25 13:37:05 +0800

    更新 tcp.sh
    更换 link

--- tcp.sh
+++ tcp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clun_download() {
-cd ~ && sleep 1 && curl -sS -o clun_tcp.sh https://raw.githubusercontent.com/cluntop/sh/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
+cd ~ && sleep 1 && curl -sS -o clun_tcp.sh https://fastly.jsdelivr.net/gh/cluntop/sh@main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh
 } && clun_download
