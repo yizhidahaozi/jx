4877d6ddbb0434d44b563f87af19ec79ece94315
ClouTop <85211716+cluntop@users.noreply.github.com>
2025-02-27 23:44:59 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# https://ghfast.top/ # sleep 1
 clun_download() {
-cd ~ && curl -sS -o clun_tcp.sh https://raw.githubusercontent.com/cluntop/sh/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh $1
+cd ~ && curl -sS -o clun_tcp.sh https://gh.clun.top/github.com/cluntop/sh/raw/refs/heads/main/tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh $1
 } && clun_download $1
+# https://ghfast.top/ # sleep 1
