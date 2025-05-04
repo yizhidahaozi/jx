6a777e43289c7a485efa1499c6c18fcaeb84d598
ClunTop <85211716+cluntop@users.noreply.github.com>
2025-05-04 12:50:32 +0800

    Update tcp.sh
    Fix TCP cannot be used

--- tcp.sh
+++ tcp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 clun_download() {
 sudo su
+
 cd ~ && curl -s https://gh.clun.top/raw.githubusercontent.com/cluntop/sh/refs/heads/main/tcp.sh -o clun_tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh $1
 } && clun_download $1
