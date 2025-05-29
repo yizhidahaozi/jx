4572dbb34585fa5e1b01806eff5fa79fa743b21f
ClunTop <85211716+cluntop@users.noreply.github.com>
2025-05-30 02:40:24 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -6,5 +6,6 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 clun_download() {
-cd ~ && curl -s https://gh.clun.top/raw.githubusercontent.com/cluntop/sh/refs/heads/main/tcp.sh -o clun_tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh $1
-} && clun_download $1
+cd ~ && curl -s https://gh.clun.top/raw.githubusercontent.com/cluntop/sh/refs/heads/main/tcp.sh -o clun_tcp.sh && chmod +x clun_tcp.sh && ./clun_tcp.sh $1 }
+
+clun_download $1
