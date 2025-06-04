6e09333a5a0867f744d88229959b72ca5aba6a92
ClunTop <85211716+cluntop@users.noreply.github.com>
2025-06-04 20:42:21 +0800

    Update get_iptv

--- py/get_iptv.py
+++ py/get_iptv.py
@@ -17,6 +17,7 @@ tv_urls = [
     "https://live.zbds.top/tv/iptv4.txt",
     "https://live.zbds.top/tv/iptv6.txt",
     "https://raw.githubusercontent.com/wind005/TVlive/refs/heads/main/m3u/%E6%B9%96%E5%8D%97%E7%A7%BB%E5%8A%A8.m3u",
+    "https://raw.githubusercontent.com/hanhan8127/TVBox/refs/heads/main/live.txt",
 ]
 
 def parse_template(template_file):
