189d0b91ebead7cc39e543f64fd9f044bf8a74f2
cluntop <85211716+cluntop@users.noreply.github.com>
2025-08-17 13:09:38 +0800

    Update Up

--- git.sh
+++ git.sh
@@ -6,12 +6,23 @@ if [ "$(id -u)" -ne 0 ]; then
  exit 1
 fi
 
+file_pwd=$(pwd)
+file="/data/data/bin.mt.plus/home/tvbox"
+
+if [ "$file_pwd" != "$file" ]; then
+    cd "$file"
+fi
+
 branch() {
    git pull origin main
 }
 
 state(){
     git status
+}
+
+warehouse() {
+    git remote add origin https://github.com/cluntop/tvbox.git
 }
 
 submit() {
@@ -31,15 +42,17 @@ echo "脚本路径：$(pwd)"
 echo -e "\n请选择要执行的操作"
 echo "1. 提交更改"
 echo "2. 远程分支"
-echo "3. 查看状态"
-echo "4. 清理垃圾"
+echo "3. 远程仓库"
+echo "4. 查看状态"
+echo "5. 清理垃圾"
 echo "0. 退出菜单"
 read -p "您的选项：" num
 case $num in
     1) submit ;;
     2) branch ;;
-    3) state ;;
-    4) garbage ;;
+    3) warehouse ;;
+    4) state ;;
+    5) garbage ;;
     0) echo -e "\n退出选项" ; exit 0 ;;
     *) echo -e "\n无效选项" ;;
     esac ; read -p $'\n返回菜单' -n 1 -r
